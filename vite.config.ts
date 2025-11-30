19b00fd3618d271f93e55418aab386a5d936ef6a
Ruan Heyns <ruan.heyns@utilifeed.com>
2025-11-30 17:50:32 +0100

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { fileURLToPath, URL } from 'node:url'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    devtools(),
+    netlify(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
